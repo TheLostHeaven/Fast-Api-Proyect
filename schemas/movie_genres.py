b5f24b9432900a4e7101d3655ee1a14ddd49ca06
Victor <vimatoca050106@gmail.com>
2023-05-25 16:33:05 -0500

    movie_genres

--- schemas/movie_genres.py
+++ schemas/movie_genres.py
@@ -1,12 +1,4 @@
 from pydantic import BaseModel, Field
-<<<<<<< HEAD
-from typing import Opcional 
-
-class Moviegenres(BaseModel):
-    id : Opcional[int] = None
-    gen_id : int = Field(ge=1, description="id de genero")
-    movie_id : int = Field(gen=1, description="Llave foranea de la pelicula")
-=======
 from typing import Optional
 
 class MovieGenres(BaseModel):
@@ -14,7 +6,6 @@ class MovieGenres(BaseModel):
     id : Optional[int] = None
     gen_id : int = Field(ge=1,description="id de genero")
     movie_id : int = Field(ge=1,description="llave foranea de pelicula")
->>>>>>> schemas/reviewers
 
     class Config:
         schema_extra = {
