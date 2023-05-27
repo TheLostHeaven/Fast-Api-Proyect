a127ae7699da5d6c307e9e01b2888e0236bdbc82
Daniel <dannyking07@outlook.es>
2023-05-27 15:03:45 -0500

    add line code

--- routers/genres.py
+++ routers/genres.py
@@ -1,16 +1,28 @@
 from fastapi import APIRouter, Path, Query
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, JSONResponse
 
 genrens_router = APIRouter()
 
+#Funcion to check the router
 @genrens_router.get('/genres', tags=['genres'], status_code=200)
 def get_router():
     return HTMLResponse('<h1>Hola desde la ruta de genres</h1>')
 
+#funcion que trae todos los generos que esta en servicios 
 @genrens_router.get('/genres', tags=['genres'], status_code=200)
 def get_router():
     return JSONResponse(content={"message":"genre created successfully"})
 
+#llamar una funcion que va a estar en el servicios 
 @genrens_router.post('/genres', tags='genres', status_code=201)
 def create_genres():
     return JSONResponse(content={"message":"genre created successfully"})
+
+#creamos un get que trae un solo genero por id
+@genrens_router.get('/genres/{genres_id}', tags=['genres'], status_code=200)
+def get_genres(genres_id: int):
+    genre = genrens_router.get_genre(genres_id)
+    return genre
+
+
+# para el genres delete debemos verificar que el id existe y despues ese genero lo eliminamos
