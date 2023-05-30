79f8ed8dcda2fc4374cb88aa4bdb7c0df0ee9760
Julian <juligaitan578@gmail.com>
2023-05-30 10:28:50 -0500

    update

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from routers.movie import movie_router
 from routers.genres import genres_router
 from routers.director import director_router 
 from routers.actor import actor_router
+from routers.movie_cast import movie_cast_router
 
 
 app = FastAPI()
@@ -20,6 +21,7 @@ app.include_router(movie_router)
 app.include_router(genres_router)
 app.include_router(director_router)
 app.include_router(actor_router)
+app.include_router(movie_cast_router)
 
 
 Base.metadata.create_all(bind=engine)
