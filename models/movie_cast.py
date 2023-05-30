8878ca97865dab0906b1c943831ea9244e7ddafd
Daniel <dannyking07@outlook.es>
2023-05-30 15:01:48 -0500

    Fix

--- models/movie_cast.py
+++ models/movie_cast.py
@@ -1,10 +1,13 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from config.database import Base
 
+from models.actor import Actor
 
 class Movie_cast(Base):
 
     __tablename__ ="movie_cast"
-    id = Column(Integer, primary_key = True)
-    mov_id = Column(Integer,ForeignKey("movie.id"))
+    
+    id = Column(Integer, primary_key = True, index=True)
+    actor_id = Column(Integer, ForeignKey("actor.id"))
+    movie_id = Column(Integer,ForeignKey("movie.id"))
     role = Column(String)
