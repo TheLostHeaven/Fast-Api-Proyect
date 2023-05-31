bde6ae224db9eb898a5404c56e675bd5fb96a307
Victor <vimatoca050106@gmail.com>
2023-05-31 15:54:31 -0500

    arreglo de codigo

--- models/reviewers.py
+++ models/reviewers.py
@@ -1,4 +1,4 @@
-from sqlalchemy import column, Integer, String
+from sqlalchemy import Column, Integer, String
 
 from config.database import Base
 
@@ -6,5 +6,5 @@ class Reviewers(Base):
 
     __tablename__ ='reviewers'
 
-    rev_id = column(Integer,primary_key=True)
-    rev_name = column(String)
+    rev_id = Column(Integer,primary_key=True)
+    rev_name = Column(String)
